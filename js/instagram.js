4eabe6b35009f9ef01193fc1f17a30b3649f6eb7
chuhemiao <535101602@qq.com>
2016-09-05 18:22:17 +0800

    Site updated: 2016-09-05 18:22:16

--- js/instagram.js
+++ js/instagram.js
@@ -18,14 +18,18 @@ var Instagram = (function(){
 			for(var i=0,len=data[em].srclist.length;i<len;i++){
 				liTmpl += '<li>\
 								<div class="img-box">\
-									<a class="img-bg" rel="example_group" href="'+data[em].bigSrclist[i]+'" title="'+data[em].text[i]+'"></a>\
-									<img lazy-src="'+data[em].srclist[i]+'" alt="">\
+									<a class="img-bg" rel="example_group" href="'+data[em].bigSrclist[i]+'" title="'+data[em].text[i]+'">\
+									<img lazy-src="'+data[em].srclist[i]+'" alt=""></a>\
 								</div>\
-							</li>';
+							</li>';		
 			}
-			$('<section class="archives album"><h1 class="year">'+data[em].year+'<em>'+data[em].month+'月</em></h1>\
-				<ul class="img-box-ul">'+liTmpl+'</ul>\
-				</section>').appendTo($(".instagram"));
+
+			
+			$('<section class="archives album"><span class="date-icon"></span><h1 class="year">'+data[em].year+'<em>'+data[em].month+'月</em></h1>\
+			<ul class="img-box-ul">'+liTmpl+'</ul>\
+			</section>').appendTo($(".instagram"));
+			
+
 		}
 
 		$(".instagram").lazyload();
@@ -42,7 +46,7 @@ var Instagram = (function(){
 		if(str.indexOf("outbound-distilleryimage") >= 0 ){
 			var cdnNum = str.match(/outbound-distilleryimage([\s\S]*?)\//)[1];
 			var arr = str.split("/");
-			return "http://distilleryimage"+cdnNum+".ak.instagram.com/"+arr[arr.length-1];
+			return "https://distilleryimage"+cdnNum+".ak.instagram.com/"+arr[arr.length-1];
 		}else{
 			var url = "http://photos-g.ak.instagram.com/hphotos-ak-xpf1/";
 			var arr = str.split("/");
@@ -56,9 +60,21 @@ var Instagram = (function(){
 			var d = new Date(data[i].created_time*1000);
 			var y = d.getFullYear();
 			var m = d.getMonth()+1;
-			var src = replacer(data[i].images.low_resolution.url);
-			var bigSrc = replacer(data[i].images.standard_resolution.url);
-			var text = data[i].caption ? data[i].caption.text : ''; // data[i].caption 有可能为 null
+			var da = d.getDate();
+			// console.log("tags:"+data[i].tags);
+			// console.log("likes:"+data[i].likes.count);
+			// console.log("low:"+data[i].images.low_resolution.url);
+			// var src = replacer(data[i].images.low_resolution.url);
+			var src = data[i].images.low_resolution.url;
+			// console.log("stanrdard:"+data[i].images.standard_resolution.url);
+			// var bigSrc = replacer(data[i].images.standard_resolution.url); 
+			var bigSrc = data[i].images.standard_resolution.url; 
+			//var text = data[i].caption.text;
+			if(data[i].caption.text==null){
+				var text = "no description by Totoro";
+			}else{
+				var text = data[i].caption.text;
+			}
 			var key = y+"-"+m;
 			if(imgObj[key]){
 				imgObj[key].srclist.push(src);
@@ -68,6 +84,7 @@ var Instagram = (function(){
 				imgObj[key] = {
 					year:y,
 					month:m,
+					date:da,
 					srclist:[bigSrc],
 					bigSrclist:[bigSrc],
 					text:[text]
@@ -78,11 +95,13 @@ var Instagram = (function(){
 	}
 
 	var getList = function(url){
-		$(".open-ins").html("图片来自instagram，正在加载中…");
+		$(".open-ins").html("图片来自我的Instagram，访问需要翻墙哦…");
+		// alert("hello begin!");
 		$.ajax({
 			url: url,
-			type:"GET",
-			dataType:"jsonp",
+			type: "GET",
+			dataType: "jsonp",
+			timeout: 8000,
 			success:function(re){
 				if(re.meta.code == 200){
 					_collection = _collection.concat(re.data);
@@ -90,24 +109,57 @@ var Instagram = (function(){
 					if(next){
 						getList(next);
 					}else{
-						$(".open-ins").html("图片来自instagram，点此访问");
+						$(".open-ins").html("图片来自我的Instagram，点此访问");
 						ctrler(_collection);
 					}
 				}else{
-					alert("access_token timeout!");
+					 alert("hello success!");
 				}
-			}
+			},
+			error:function(){  
+    			//alert("hello error");
+    			$('<section><h1 class="warncontainer">BAD REQUEST</h1><p class="warncontainer1"><strong>Dear friend, I am so sorry that your request has been blocked by the damned Great Fire Wall. Fxxk the GFW</strong></p><p id="suggestions">If you are still trying to visit this page,<a href="https://getlantern.org/">click here</a> to download <strong>Lantern</strong> to visit over the <strong>Great Fire Wall</strong>.<p></section>').appendTo($(".instagram")); 
+			},
+/*			error:function(XMLHttpRequest, textStatus, errorThrown){
+		        alert(XMLHttpRequest.status);
+                alert(XMLHttpRequest.readyState);
+                alert(textStatus);
+				$('<div class="warncontainer">
+				      <h1>Bad Request!</h1>
+				      <p><strong>Dear friend, I am so sorry that your request has been blocked by the evil Great Fire Wall.</strong></p>
+				      <p>
+				        If you are still trying to visit this page,
+				        <a href="http://www.zhiyanblog.com/goagent-chrome-switchyomega-proxy-2015-latest.html">click here</a> to learn how to surf the Internet over the <strong>Great Fire Wall</strong>
+				        or <a href="http://xskywalker.software.informer.com/">click here</a> to download <strong>xskywalker</strong> to visit.
+				      </p>').appendTo($(".instagram"));
+				alert("hello error!");
+			},*/                    
+			complete:function(XMLHttpRequest, textStatus) {
+                
+                //alert("hello welcome!");
+            }
+
 		});
 	}
 	
 
 	var changeSize = function(){	
 		if($(document).width() <= 600){
-			$(".img-box").css({"width":"auto", "height":"auto"});
+			var width = $(".img-box-ul").width();
+			var size = Math.max(width*0.35, 137);
+			$(".img-box").width(size).height(size);
+			$(".img-box").css({"margin-bottom":"10px"});
+			$(".open-ins").css({"font-size":"10px"}); //缩小字体避免换行			
+			// $(".description").css({"font-size":"12px"}); //副标题
+			// $("#logo").css({"font-size":"35px"}); //标题
+			//$(".img-box").css({"width":"auto", "height":"auto"});
 		}else{
 			var width = $(".img-box-ul").width();
 			var size = Math.max(width*0.26, 157);
 			$(".img-box").width(size).height(size);
+			$(".open-ins").css({"font-size":"15px"}); 
+			// $(".description").css({"font-size":"18px"}); //副标题
+			// $("#logo").css({"font-size":"55px"}); //标题
 		}
 	}
 
@@ -119,20 +171,19 @@ var Instagram = (function(){
 
 	return {
 		init:function(){
-			//getList("https://api.instagram.com/v1/users/438522285/media/recent/?access_token=438522285.2082eef.ead70f432f444a2e8b1b341617637bf6&count=100");
-			//var 	insid = $(".instagram").attr("662189594c964c10841f86feea9c76bd"),
-			//	userId = $(".instagram").attr("3854252896");
-
-			//if(!insid){
-			//	alert("Didn't set your instagram client_id.\nPlease see the info on the console of your brower.");
-			//	console.log("Please open 'http://instagram.com/developer/clients/manage/' to get your client-id.");
-			//	return;
-			//}
-			getList("https://api.instagram.com/v1/users/3854252896/media/recent/?access_token=3854252896.6621895.95b8fcb7daeb490793b92707e39a0791&count=100");
+			//getList("https://api.instagram.com/v1/users/2059681846/media/recent/?access_token=2059681846.4f38fd6.b4d5736e2ec1402190815007124c66b8&count=100");
+			// var insid = $(".instagram").attr("data-client-id");  $(".img-box").css({"margin-bottom":"10px","margin-bottom":"10px"});
+			// if(!insid){
+			// 	alert("Didn't set your instagram client_id.\nPlease see the info on the console of your brower.");
+			// 	console.log("Please open 'https://instagram.com/developer/clients/manage/' to get your client-id.");
+			// 	return;
+			// }
+			getList("https://api.instagram.com/v1/users/self/media/recent/?access_token=3854252896.6621895.95b8fcb7daeb490793b92707e39a0791&count=100");
 			bind();
 		}
 	}
 })();
 $(function(){
+	$('<a href="https://instagram.com/chehemiao" target="_blank" class="open-ins" style="float:right;font-size:15px;color:#6E7173;margin-top:10px">图片来自我的Instagram，点此访问</a>').appendTo($(".post-title"));
 	Instagram.init();
 })
